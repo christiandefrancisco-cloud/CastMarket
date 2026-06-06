618a5e8a625193cdc17e35573d600e109cbcdb3c
christiandefrancisco-cloud <christiandefrancisco@gmail.com>
2026-06-05 21:07:36 -0300

    Delete api/config.js

--- api/config.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Content-Type', 'application/javascript');
-  res.send(`
-    window.__CM_ANTHROPIC_KEY__ = "${process.env.ANTHROPIC_KEY || ''}";
-    window.__CM_ELEVENLABS_KEY__ = "${process.env.ELEVENLABS_KEY || ''}";
-  `);
-}
